ae7f25d2796778b454dce163fa75fa1a8828879a
marcusgeorgievski <marcusgeorgievski@outlook.com>
2025-09-09 23:55:45 -0400

    domain text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="font-mono text-5xl">
-          Detour<span className="opacity-10">.app</span>
+          Detour<span className="opacity-10">app.io</span>
         </h1>
         <ol className="font-mono list-inside list-disc text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
